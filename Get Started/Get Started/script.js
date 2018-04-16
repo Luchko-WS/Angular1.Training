bc8f8906a11b485e6c0a8a0f306ff8c545b3e618
andrii.luchko <andrii.luchko@udelphi.com>
2018-04-16 13:19:25 +0300

    GetStarted - Directives and Views

--- Get Started/Get Started/script.js
+++ Get Started/Get Started/script.js
@@ -1,22 +1,34 @@
 // Code goes here
 (function(){
 
-    var app = angular.module("gitHubViewer", []);
+    var app = angular.module('gitHubViewer', []);
     
     var MainCtrl = function($scope, $http){
         
         var onUserComplete = function(response){
             $scope.user = response.data;
+			$http.get($scope.user.repos_url)
+				.then(onRepos, onError);
         };
         
+		var onRepos = function(response){
+			$scope.repos = response.data;
+			console.log($scope.repos);
+		};
+		
         var onError = function(reason){
+			$scope.user = null;
+			console.log($scope.user);
             $scope.error = "Couldn't fetch the user";
         };
         
-        $http.get("https://api.github.com/users/Luchko-WS")
+		$scope.search = function(username){
+			        $http.get('https://api.github.com/users/' + username)
             .then(onUserComplete, onError);
-        
-        $scope.message = "Hello, Angular!";
+		};
+		
+        $scope.message = 'GitHub Viewer';
+		$scope.repoSortOrder = '-created_at';
     };
     
     app.controller("MainCtrl", ["$scope", "$http", MainCtrl])
